9b92d3a9281c65109c8139a7257939b4d438507e
Viktoriya Savaryn <37408680+plyus1313@users.noreply.github.com>
2018-05-23 13:23:50 +0200

    Update script.js

--- background-generator/script.js
+++ background-generator/script.js
@@ -10,30 +10,9 @@ function setGradient() {
     + ", "
     + color2.value
     + ")";
-
-css.textContent = body.style.background + ";";
+ 
+    css.textContent = body.style.background + ";";
 }
-
-setGradient();
-
- function getRandomColor() {
-//     var letters = '0123456789ABCDEF';
-//     var color = '#';
-//     for (var i = 0; i < 6; i++) {
-//       color += letters[Math.round(Math.random() * 16)];
-//
-color1.value= getRandomNumber();
-color2.value= getRandomNumber() ;
-}
-//     return color;
-//   }
-//
-// function setRandomColor() {
-//     $(".color1").val(getRandomColor());
-//     $(".color2").val(getRandomColor());
-//     setGradient();
-  // }
 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
